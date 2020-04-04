8f52b1ab1c1f8a0d58d0d7018c4b4c36005f7169
Gabriel Nascimento <gabrielgrames@gmailcom>
2020-04-04 15:52:56 -0400

    Exception OK

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from "react";
+import swal from 'sweetalert';
 
 import { Container, Picture, TextBox, Link } from "./styles";
 
 const Info = ({ username }) => {
   const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
+  const [found, setFound] = useState(false); 
 
   async function getUser(username){
-    const data = await fetch(`https://api.github.com/users/${username}`);
-    data.json().then( (response) => setUser(response));
-    const reposData = await fetch(`https://api.github.com/users/${username}/repos`);
-    reposData.json().then( (response) => {
-      response.sort((a, b) => {
-        if (a.created_at < b.created_at) {
-          return 1;
+    try {
+      setFound(true);
+      const data = await fetch(`https://api.github.com/users/${username}`);
+      data.json().then( (response) => setUser(response));
+      const reposData = await fetch(`https://api.github.com/users/${username}/repos`);
+      reposData.json().then( (response) => {
+        try {
+          response.sort((a, b) => {
+            if (a.created_at < b.created_at) {
+              return 1;
+            }
+            if (a.created_at > b.created_at) {
+              return -1;
+            }
+            return 0;
+          });
+          setRepos(response.slice(0,4))
+          
+        } catch (error) {
+          swal("User not found");
+          setFound(false);
         }
-        if (a.created_at > b.created_at) {
-          return -1;
-        }
-        return 0;
       });
-      setRepos(response.slice(0,4))
-    });
+    } catch (error) {
+      swal("User not found");
+    }
+    
     
   } 
 
@@ -33,7 +47,7 @@ const Info = ({ username }) => {
           Name: {user.name}<br/>Username: {user.login}<br/>Followers: {user.followers}<br/>Repositories count: {user.public_repos}
         </TextBox>
         <TextBox>
-        Newest Repositories:<br/>{ repos.map(element => { return ( <div> <Link href={`https://github.com/${username}/${element.name}`}> {element.name} </Link> <br/> </div>)})}
+        Newest Repositories:<br/>{ repos.map(element => { return ( <div key={element.id}> <Link  target="_blank" href={`https://github.com/${username}/${element.name}`}> {element.name} </Link> <br/> </div>)})}
         </TextBox>
       </div>
       
@@ -48,7 +62,7 @@ const Info = ({ username }) => {
 
   return(
     <>
-      {user !== {} && userInfo(user)}
+      {(user !== {} & found === true) ? userInfo(user) : null }
     </>
   );
 }
